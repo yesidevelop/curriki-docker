58e9ffef2d70cf0e9cd186de630becf9bdeea419
IMCoder <imcoder001@gmail.com>
2020-11-19 16:06:32 +0500

    updated docker file for permissions

--- entrypoint.client.sh
+++ entrypoint.client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp /var/app/current/env/.env.local /var/app/current/.env.local
+# cp /var/app/current/env/.env.local /var/app/current/.env.local
 #npm run start:dev
 npm run build
 npm run start:prod
