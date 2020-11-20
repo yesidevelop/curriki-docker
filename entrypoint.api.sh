d4320f6631f8d0f589eebaae3e8394dc15b9033d
IMCoder <imcoder001@gmail.com>
2020-11-20 11:51:21 +0500

    commented key generate

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -2,11 +2,11 @@
 
 # cp /var/www/html/env/.env /var/www/html/.env
 php /var/www/html/artisan config:cache
-php /var/www/html/artisan key:generate
+# php /var/www/html/artisan key:generate
 php /var/www/html/artisan passport:install
 php /var/www/html/artisan storage:link
 # redis-server &
-laravel-echo-server start --force &
+laravel-echo-server start &
 php /var/www/html/artisan queue:work --timeout=0 &
 # php /var/www/html/artisan config:cache
 # chmod 777 -R /var/www/html/storage
