6bbb03f0a294577f0e42d8dc102d11963519a6ef
Mike Hepburn <eformat@gmail.com>
2020-11-20 19:09:10 +1000

    🦩 pod reorganisation 🦩

--- api-init-container.sh
+++ api-init-container.sh
@@ -3,10 +3,6 @@
 STORAGE_ZIP=${STORAGE_ZIP:-https://github.com/ActiveLearningStudio/curriki-eks/raw/develop/storage/storage.zip}
 
 if test ! -f '/var/www/html/storage/storage.zip'; then
-    curl -o '/var/www/html/storage/storage.zip' "${STORAGE_ZIP}"
+    curl --compressed -o '/var/www/html/storage/storage.zip' "${STORAGE_ZIP}"
+    cd /var/www/html/storage && unzip -q storage.zip
 fi
-
-php /var/www/html/artisan config:cache
-php /var/www/html/artisan key:generate
-php /var/www/html/artisan passport:install
-php /var/www/html/artisan storage:link
