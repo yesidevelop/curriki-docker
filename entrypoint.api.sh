6d70549016024dbaf4f5cf0aba400b94208ab3a1
IMCoder <imcoder001@gmail.com>
2020-11-20 07:44:11 +0500

    updated api entrypoint for infinity timeout and commented some

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp /var/www/html/env/.env /var/www/html/.env
+# cp /var/www/html/env/.env /var/www/html/.env
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan key:generate
 php /var/www/html/artisan passport:install
 php /var/www/html/artisan storage:link
-php /var/www/html/artisan config:cache
-chmod 777 -R /var/www/html/storage
-service apache2 restart
-while true; do sleep 10000000 ; done
+# php /var/www/html/artisan config:cache
+# chmod 777 -R /var/www/html/storage
+# service apache2 restart
+while true; do sleep infinity ; done
