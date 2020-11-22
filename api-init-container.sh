2d4d7631366fc31c0729877338f9c1e7b3814e20
IMCoder <imcoder001@gmail.com>
2020-11-23 00:00:21 +0500

    Updated compress commands

--- api-init-container.sh
+++ api-init-container.sh
@@ -3,6 +3,6 @@
 STORAGE_ZIP=${STORAGE_ZIP:-https://github.com/ActiveLearningStudio/curriki-eks/raw/develop/storage/storage.zip}
 
 if test ! -f '/var/www/html/storage/storage.zip'; then
-    curl -L -O --compressed -o '/var/www/html/storage/storage.zip' "${STORAGE_ZIP}"
+    curl -L --compressed -o '/var/www/html/storage/storage.zip' "${STORAGE_ZIP}"
     cd /var/www/html/storage && unzip -q storage.zip
 fi
