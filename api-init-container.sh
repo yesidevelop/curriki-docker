32f85b278b53ef4ee46e3b3007e6f4e06ba38046
Mike Hepburn <eformat@gmail.com>
2020-11-23 07:47:18 +1000

    🦄 fix raw url 🦄

--- api-init-container.sh
+++ api-init-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STORAGE_ZIP=${STORAGE_ZIP:-https://github.com/ActiveLearningStudio/curriki-eks/raw/develop/storage/storage.zip}
+STORAGE_ZIP=${STORAGE_ZIP:-https://raw.githubusercontent.com/ActiveLearningStudio/curriki-eks/develop/storage/storage.zip}
 
 if test ! -f '/var/www/html/storage/storage.zip'; then
     curl -L --compressed -o '/var/www/html/storage/storage.zip' "${STORAGE_ZIP}"
