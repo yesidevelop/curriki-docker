8675cae414944aef72cdc20089909255f76f8b30
Faisal Masood <fmasood@redhat.com>
2020-11-26 15:50:46 +1100

    remove the restart

--- api-entrypoint.sh
+++ api-entrypoint.sh
@@ -4,5 +4,5 @@ php /var/www/html/artisan config:cache
 # php /var/www/html/artisan key:generate
 php /var/www/html/artisan passport:install
 php /var/www/html/artisan storage:link
-apache2ctl restart
+
 apache2ctl -D FOREGROUND
